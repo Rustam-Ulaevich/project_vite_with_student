d714490690f29f6d393cbf44cc04f6ffd868cfba
Rustam-Ulaevich <praporglamur@gmail.com>
2026-03-19 18:47:56 +0300

    error correction

--- project_vite_ with_condratev/src/components/Content.tsx
+++ project_vite_ with_condratev/src/components/Content.tsx
@@ -1,31 +1,45 @@
 import audio1 from '../audio/viktor-cojj-zvezda-po-imeni-solnce.mp3'
+import audio2 from '../audio/abba-happy-new-year.mp3'
+import audio3 from '../audio/ANNA_ASTI_-_Po_baram.mp3'
 
 export const Content = () => {
-
+	
 	const traсks = [
-		{id: 1, title: 'Pop', genre: 'Pop', url: {audio1}},
-		{id: 2, title: 'Rock', genre: 'Rock', url: ''},
-		{id: 3, title: 'Rock', genre: 'Rock', url: ''},
-		{id: 4, title: 'Rock', genre: 'Rock', url: ''}
+		{id: 1, title: 'Viktor Cojj', genre: 'Rock', url: audio1},
+		{id: 2, title: 'Abba', genre: 'Hit80', url: audio2},
+		{id: 3, title: 'Anna Asti', genre: 'Pop', url: audio3},
 	]
+
+	if(traсks.length === 0){
+		return(
+			<div  className="content">NO TRACK!</div>
+		)
+	}
+
 
     return (
       <>
-			<div className="content">
-				<ul>
-					{traсks.map( traсk => (
-						<li className="track" key={traсk.id}>
-							<div className="track_discription">
-								<p>{traсk.title}</p>
-								<p>{traсk.genre}</p>
-							</div>
-							
-							<audio controls src='{traсk.url}'></audio>
-						</li>
-					))}
+				<div className="content">
+					<div>
+						<button>Rock</button>
+						<button>Pop</button>
+						<button>Hit80</button>
+					</div>
 
-				</ul>
-			</div>
+					<ul>
+					{traсks.map( traсk => (
+							<li className="track" key={traсk.id}>
+								<div className="track_discription">
+									<p>{traсk.title}</p>
+									<p>{traсk.genre}</p>
+								</div>
+
+								<audio controls src={traсk.url}></audio>
+							</li>
+						))}
+
+					</ul>
+				</div>
 				
 			</>
     )
