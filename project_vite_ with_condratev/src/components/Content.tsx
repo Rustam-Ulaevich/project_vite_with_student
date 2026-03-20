4a80d0c885c36e269e3d2b1bc1c9b19c8090d411
Rustam-Ulaevich <praporglamur@gmail.com>
2026-03-20 21:23:37 +0300

    create fetch request

--- project_vite_ with_condratev/src/components/Content.tsx
+++ project_vite_ with_condratev/src/components/Content.tsx
@@ -1,14 +1,46 @@
 import audio1 from '../audio/viktor-cojj-zvezda-po-imeni-solnce.mp3'
 import audio2 from '../audio/abba-happy-new-year.mp3'
 import audio3 from '../audio/ANNA_ASTI_-_Po_baram.mp3'
+import { useEffect, useState } from 'react'
 
 export const Content = () => {
-	
-	const traсks = [
+
+	const[traсks, setTracks] = useState([
 		{id: 1, title: 'Viktor Cojj', genre: 'Rock', url: audio1},
 		{id: 2, title: 'Abba', genre: 'Hit80', url: audio2},
 		{id: 3, title: 'Anna Asti', genre: 'Pop', url: audio3},
-	]
+	])
+	
+
+	useEffect( () => {
+        (async () => {
+            try {
+                const data = await fetch('https://62c3ffff7d83a75e39ecd122.mockapi.io/orders') // Необходимо создать собственный подобный сервис Mockapi.io
+								.then(res => res.json())
+								.then(json => console.log(json))
+                
+            } catch (error) {
+                alert('Error when requesting orders')
+                console.log(error)
+            }
+        })();
+    }, [])
+
+	// fetch('https://api.ittybit.com/tasks', {
+  // 	method: 'POST',
+  // 	headers: {
+  //   	'Authorization': 'Bearer <ITTYBIT_API_KEY>',
+  //   	'Content-Type': 'application/json',
+  // 	},
+  // 	body: JSON.stringify({
+  //   	url: 'https://webapp.ittybit.net/homepage-dance.mp4',
+  //   	kind: 'audio',
+  //   	options: { format: 'mp3' },
+  // 	})
+	// }).then(res => res.json())
+	// .then(json => console.log(json))
+	
+	
 
 	if(traсks.length === 0){
 		return(
--- project_vite_ with_condratev/src/components/Navbar.tsx
+++ project_vite_ with_condratev/src/components/Navbar.tsx
@@ -13,7 +13,7 @@ export const Navbar = () => {
         
         <ul>
           <li><a href="http://google.com">Main</a></li>
-          <li><a href="http://xn----7sbnjgaaco2agjzw7hxb.xn--p1ai/vacancies/afrikakorpus?utm_source=yandex&utm_medium=cpc&utm_campaign=707719711&mango=|c:707719711|g:5724129990|b:1903178616426774918|k:205724129990|st:context|a:no|s:rus.hitmotop.com|t:none|p:0|r:205724129990|reg:10761|net:{yad}&yclid=3460780122883751935">Favorit</a></li>
+          <li><a href="http://">Favorit</a></li>
           <li><a href="http://">Contacts</a></li>
           <li><a href="http://">Orders</a></li>
         </ul>      
