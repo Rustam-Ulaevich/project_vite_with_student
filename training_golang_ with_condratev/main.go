6afff08d36f249679cf72dfde91c6c142516ecb2
Rustam-Ulaevich <praporglamur@gmail.com>
2026-05-20 17:29:58 +0300

    structures, add methods

--- training_golang_ with_condratev/main.go
+++ training_golang_ with_condratev/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"go/types"
 )
 
-
 // func main() {
-	// Переменные :=
-	/*
+// Переменные :=
+/*
 	score := 0.0
 	text := "Get ready!!!"
 	text2 := "Your score: "
@@ -23,17 +23,17 @@ import (
 	fmt.Println(text3)
 	fmt.Println(text2, score)
 
-	score := 50 
-	*/
+	score := 50
+*/
 
-	// Условное ветвление if else
-	/*
+// Условное ветвление if else
+/*
 	if score > 10 {
 		if score > 15 {
 			fmt.Println("You megasupermen!!!")
 		} else {
 			fmt.Println("You good men!")
-		}	
+		}
 	}else{
 		fmt.Println("You need, very work...")
 	}
@@ -61,10 +61,10 @@ import (
 	}
 
 	if score != 7 {fmt.Println("...")}
-	*/
+*/
 
-	// Цикл for
-	/*
+// Цикл for
+/*
 	score := 2
 	fmt.Println(score)
 
@@ -85,15 +85,15 @@ import (
 		if rand.Intn(2000) == 1 {
 			fmt.Println("The end!")
 			break
-		}			
+		}
 	}
-	
+
 	fmt.Println(score)
 
-	*/
+*/
 
-	// Function
-	/*
+// Function
+/*
     res := plus(1, 2)
     fmt.Println("1+2 =", res)
 
@@ -112,64 +112,123 @@ import (
 	fmt.Println("После вызова функции")
 
 	fmt.Println("number:", number)
-	*/
+*/
 
-	// Defer
-	/*
-    path := filepath.Join(os.TempDir(), "defer.txt")
-    f := createFile(path)
-    defer closeFile(f)
-    writeFile(f)
-	*/
+// Defer
+/*
+   path := filepath.Join(os.TempDir(), "defer.txt")
+   f := createFile(path)
+   defer closeFile(f)
+   writeFile(f)
+*/
 
-	// Указатели
-	/*
+// Указатели
+/*
 	b := 5
 	a := &b
 
 	fmt.Println(a, *a)  // * разименование указателя
-	
+
 	var ptr *int  // nil указатель
 
 	fmt.Print(ptr)
-	*/
-	
-	// Structures
+*/
 
-	type User struct {
-		Name string			// ""
-		Age int				// 0
-		PhoneNumber string	// ""
-		IsClose bool		// false
-		Rating float64		// 0.0
+// Structures
+
+type User struct {
+	Name        string  // ""
+	Age         int     // 0
+	PhoneNumber string  // ""
+	IsClose     bool    // false
+	Rating      float64 // 0.0
+}
+
+func (u User) RatingUp(rating float64) {
+	if u.Rating+rating <= 10 {
+		u.Rating += rating
+		fmt.Println("Рейтинг стал:", u.Rating)
+	} else {
+		fmt.Println("Не пройдена валидация")
+	}
+}
+
+func (u *User) ChangeName(newName string) {
+	if newName != "" {
+		u.Name = newName
+	} else {
+		fmt.Println("Получено пустое имя")
+	}
+}
+
+func (u *User) ChangeAge(newAge int) {
+	if newAge > 0 && newAge < 150 {
+		u.Age = newAge
+	} else {
+		fmt.Println("Получено не валидное число")
+	}
+}
+
+func (u *User) RatingUpp(value float64) {
+	if u.Rating + value <= 10.0 {
+		u.Rating += value
+	}
+}
+
+func (u *User) RatingDown(value float64) {
+	if u.Rating - value >= 0.0 {
+		u.Rating -= value
+	}
+}
+
+func (u *User) ChangeCloseAccount() {
+	u.IsClose = !u.IsClose
+}
+
+func NewUser(
+	name string,
+	age int,
+	phoneNumber string,
+	isClose bool,
+	rating float64,
+) User{
+	if name == "" {
+		return User{}
+	}
+	if age <= 0 || age >= 150 {
+		return User{}
+	}
+	if phoneNumber == "" {
+		return User{}
+	}
+	// if isClose != types.IsBoolean {
+	// 	return  User{} 
+	// }
+	if rating < 0.0 || rating > 10.0 {
+		return User{}
 	}
 
-	func (u User) RatingUp(rating float64){
-		if u.Rating + rating <= 10{
-			u.Rating += rating
-			fmt.Println("Рейтинг стал:", u.Rating)
-		}else {
-			fmt.Println("Не пройдена валидация")
-		}
+	return User{
+		Name: name,
+		Age: age,
+		PhoneNumber: phoneNumber,
+		IsClose: isClose,
+		Rating: rating,
 	}
+}
 
-	func main() {
-  
-		user := User{
-			Name: "Dima",
-			Age: 32,
-			PhoneNumber: "+7 999 1234564",
-			IsClose: true,
-			Rating: 6.1,
-		}
+func main() {
 
-		fmt.Println("User:", user)
-		fmt.Println("User age:", user.Age)
+	user := NewUser(
+		"Dima", 32, "+7 999 1234564", true, 0.0,
+	)
 
-		user.RatingUp(4)
+	fmt.Println("User:", user)
+	fmt.Println("User age:", user.Age)
 
-	}
+	user.RatingUp(4)
 
+}
 
 // }
 
@@ -195,8 +254,3 @@ import (
 //         panic(err)
 //     }
 // }
-
-
-
-
-
