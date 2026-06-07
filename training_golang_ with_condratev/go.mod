2b0792f8d2e1bf068070f3a1cd9bafdf6e7adc59
Rustam-Ulaevich <praporglamur@gmail.com>
2026-06-07 21:07:01 +0300

    Select

--- training_golang_ with_condratev/go.mod
+++ training_golang_ with_condratev/go.mod
@@ -2,11 +2,4 @@ module study
 
 go 1.26.2
 
-require github.com/k0kubun/pp v3.0.1+incompatible
-
-require (
-	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
-	github.com/mattn/go-colorable v0.1.15 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-)
+require golang.org/x/text v0.37.0
--- training_golang_ with_condratev/main.go
+++ training_golang_ with_condratev/main.go
@@ -2,20 +2,67 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
+	
 )
 
-func mine(transferPoint chan int, n int) {
-	fmt.Println("Действие №", n, "началось...")
-	time.Sleep(1*time.Second)
-	fmt.Println("Действие №", n, "закончилось...")
+/*
+// func mine(transferPoint chan int, n int) {
+// 	fmt.Println("Действие №", n, "началось...")
+// 	time.Sleep(1*time.Second)
+// 	fmt.Println("Действие №", n, "закончилось...")
 
-	transferPoint <- 10                                // Отправка числа 10 в канал
-	fmt.Println("Действие №", n, "Число передано!!!")
-}
+// 	transferPoint <- 10                                // Отправка числа 10 в канал
+// 	fmt.Println("Действие №", n, "Число передано!!!")
+// }
+*/
 
 
 func main() {
+
+	strChanel := make(chan string)
+	intChanel := make(chan int)
+	
+	go func() {
+		i := 0
+		for{
+			intChanel <- i
+			i++
+			time.Sleep(1000 * time.Millisecond)
+		}
+	}()
+
+	go func() {
+		i := 0
+		for{
+			strChanel <- "str" + strconv.Itoa(i)
+			i++
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
+
+	// time.Sleep(5000 * time.Millisecond)
+
+	for{
+		select{
+			case number := <- intChanel:
+				fmt.Println("integer:", number)
+			case string := <- strChanel:
+				fmt.Println("string:", string)
+			// default:
+			// 	fmt.Println("Никакой канал не готов")
+		}
+		
+	}
+
+	
+
+	
+
+
+
+	/*
 	coal := 0
 
 	transferPoint := make(chan int)  // создание канала
@@ -40,4 +87,5 @@ func main() {
 
 	fmt.Println("Накоплено", coal, "единиц")
 	fmt.Println("Прошло времени:", time.Since(initTime))
+	*/
 }
